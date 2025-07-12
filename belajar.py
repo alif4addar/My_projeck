87f5eb4a9e8791cf88f6f7ccceef0cbec7e3922d
alif4addar <alifqaddar@gmail.com>
2025-07-12 09:03:27 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -13,3 +13,10 @@ st.subheader("_Streamlit_ is :blue[cool] :sunglasses:", divider="green")
 st.markdown("*Streamlit* is **really** ***cool***.")
 
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
