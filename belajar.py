4c070f69c7d6ee16b29de476e46022a12b4a3a2f
alif4addar <alifqaddar@gmail.com>
2025-07-12 09:08:56 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -21,6 +21,10 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
 
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
   
