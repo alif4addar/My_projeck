7faebe9659dad38ac08c9189f1a334bf84156e69
alif4addar <alifqaddar@gmail.com>
2025-07-12 08:49:25 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -6,7 +6,7 @@ st.write("Hello, *World!* :🖕🖕:")
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("_Streamlit_ is :blue[cool] :sunglasses:", divider="gray")
 
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
