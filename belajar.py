a446ea5808e3a270fa390c039cd29e02be90bc5a
alif4addar <alifqaddar@gmail.com>
2025-07-12 09:31:28 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -52,4 +52,12 @@ with st.sidebar:
         time.sleep(5)
     st.success("Done!")
 
+if not st.user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.user.name}!")
+
 st.success('This is a success message!', icon="✅")
