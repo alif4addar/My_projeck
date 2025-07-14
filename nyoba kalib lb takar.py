df608a31fb4518e09e7b1f949038c0fe924174ca
alif4addar <alifqaddar@gmail.com>
2025-07-14 20:11:05 +0700

    Update nyoba kalib lb takar.py

--- nyoba kalib lb takar.py
+++ nyoba kalib lb takar.py
@@ -7,6 +7,5 @@ if st.button("Tambah Data"):
 left, middle, right = st.columns(3)
 if left.button("Plain button", use_container_width=True):
     left.markdown("You clicked the plain button.")
-middle():
 if right.button("Material button", icon=":material/mood:", use_container_width=True):
     right.markdown("You clicked the Material button.")
