f0190ff5fd1373b437e6630dec501d37da2ea562
alif4addar <alifqaddar@gmail.com>
2025-07-14 20:08:45 +0700

    Update nyoba kalib lb takar.py

--- nyoba kalib lb takar.py
+++ nyoba kalib lb takar.py
@@ -4,7 +4,7 @@ import streamlit as st
 if st.button("Tambah Data"):
   st.write("Why hello there")
 
-left, right = st.columns(2)
+left, midle, right = st.columns(2)
 if left.button("Menambah Data", use_container_width=True):
     left.markdown(".")
 if right.button("Selanjutnya", icon=":material/mood:", use_container_width=True):
