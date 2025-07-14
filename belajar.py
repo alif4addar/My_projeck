cc3d1cf29fd13d42737f76034b52b03b83ee0308
alif4addar <alifqaddar@gmail.com>
2025-07-14 19:23:16 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -60,4 +60,10 @@ else:
         st.logout()
     st.write(f"Hello, {st.user.name}!")
 
-
+if not st.user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.user.name}!")
