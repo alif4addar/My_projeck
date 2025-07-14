4f5b756cdbcb6a808d828ec84643d21430004e8e
alif4addar <alifqaddar@gmail.com>
2025-07-14 20:07:56 +0700

    Update nyoba kalib lb takar.py

--- nyoba kalib lb takar.py
+++ nyoba kalib lb takar.py
@@ -4,11 +4,9 @@ import streamlit as st
 if st.button("Tambah Data"):
   st.write("Why hello there")
 
-left, middle, right = st.columns(3)
-if left.button("Plain button", use_container_width=True):
-    left.markdown("You clicked the plain button.")
-if middle.button("Emoji button", icon="😃", use_container_width=True):
-    middle.markdown("You clicked the emoji button.")
-if right.button("Material button", icon=":material/mood:", use_container_width=True):
+left, right = st.columns(2)
+if left.button("Menambah Data", use_container_width=True):
+    left.markdown(".")
+if right.button("Selanjutnya", icon=":material/mood:", use_container_width=True):
     right.markdown("You clicked the Material button.")
 
