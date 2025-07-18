5a8c2e6f2edbb20933031fe03ef49ba6c6dbd97a
alif4addar <alifqaddar@gmail.com>
2025-07-18 17:45:32 +0700

    Update nyoba kalib lb takar.py

--- nyoba kalib lb takar.py
+++ nyoba kalib lb takar.py
@@ -189,7 +189,7 @@ st.markdown("""
     /* Warna spinner number input (+/-) */
     input[type=number]::-webkit-inner-spin-button {
         background-color: #819A91; /* Warna latar tombol */
-        color: #FDFAF6; /* Warna simbol +/- (jika didukung) */
+        color: #ED3500; /* Warna simbol +/- (jika didukung) */
         border-radius: 3px;
         border: 1px solid #FDFAF6;
     }
